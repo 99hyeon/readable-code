8caecfbf955353054d382c5463927e31f70c15c6
99hyeon <hyeon99@ajou.ac.kr>
2025-03-10 14:41:44 +0900

    OCP

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,16 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
+
+public class GameApplication {
+    public static void main(String[] args) {
+        GameLevel gameLevel = new Advanced();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.run();
+    }
+
+}
