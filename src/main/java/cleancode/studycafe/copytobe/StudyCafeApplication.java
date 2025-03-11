2f8a62facdbe41ac14aab9184f6c5440dc8c5b91
99hyeon <hyeon99@ajou.ac.kr>
2025-03-11 20:35:04 +0900

    새 폴더 생성

---
+++ src/main/java/cleancode/studycafe/copytobe/StudyCafeApplication.java
@@ -0,0 +1,12 @@
+package cleancode.studycafe.copytobe;
+
+import cleancode.studycafe.copytobe.StudyCafePassMachine;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/copytobe/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.copytobe.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
